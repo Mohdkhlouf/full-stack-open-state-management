3f7c5bf5acdc268a4d1d8cfa39cb91d197cca666
Mohammad <mohammad.khlouf@gmail.com>
2026-08-29 22:21:55 +0300

    part 6.5 is done

--- anecdotes/src/components/AnecdoteList.jsx
+++ anecdotes/src/components/AnecdoteList.jsx
@@ -6,13 +6,15 @@ import { useAnecdotes, useVotesIncrement } from "../store"
 const AnecdoteList = () => {
 
   const anecdotes = useAnecdotes()
+  const sortedAnecdotes = anecdotes.toSorted((a, b) => b.votes - a.votes)
+
   const vote = useVotesIncrement()
 
   return (
     <div>
       <h2>Anecdotes</h2>
 
-      {anecdotes.map((anecdote) => (
+      {sortedAnecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
